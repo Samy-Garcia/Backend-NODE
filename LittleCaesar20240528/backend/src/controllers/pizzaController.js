938cb62efaa370e7c2968dda6e7fd58bdc5be269
CristianGuardado <20240365@ricaldone.edu.sv>
2026-05-19 09:11:29 -0600

    mas funciones crud pizzas

--- LittleCaesar20240528/backend/src/controllers/pizzaController.js
+++ LittleCaesar20240528/backend/src/controllers/pizzaController.js
@@ -52,4 +52,99 @@ pizzaController.updatePizza = async (req, res) => {
     res.json({message: "Pizza actualizada correctamente" });
 };
 
+//obtener spolo un registro
+pizzaController.getPizzasById = async (req, res) => {
+    try {
+        
+        const pizza = await pizzaModel.findById(req.params.id);
+        if(!pizza){
+            return res.status(404).json({message: "Pizza no encontrada"});
+        }
+
+        return res.status(200).json(pizza);
+
+    } catch (error) {
+        console.log("error"+ error)
+        return res.status(500).json({message: "Error al obtener la pizza"});
+    }
+};
+
+//obtener pizzas con stock bajo
+pizzaController.getLowStock = async (req, res) => {
+    try {
+        
+        const pizzas = await pizzaModel.find({stock: {$lt:5}})
+
+        if(!pizzas) {
+            return res.status(404).json({message: "no hay pizzas con stock bajo"})
+        }
+
+        return res.status(200).json(pizzas)
+
+    } catch (error) {
+        console.log("error"+ error)
+        return res.status(500).json({message: "Internal Server Error"}); 
+    }
+}
+
+//SELECT CON FILTRO
+pizzaController.getPizzasByPriceRange = async (req, res) => {
+    try {
+        
+        const {min, max} = req.body;
+
+        const pizzas = await pizzaModel.find({
+            price: {$gte: min, $lte: max}
+        })
+
+        if(!error) {
+            return re.status(404).json({message: "no hay pizzas con este rango de precio"})
+        }
+
+        return res.status(200).json(pizzas)
+
+    } catch (error) {
+        console.log("error"+ error)
+        return res.status(500).json({message: "internal Server Error"});
+    }
+}
+
+
+//contar cuantos elementos tengo
+pizzaController.countPizzas = async (req, res) => {
+    try{
+    const count = await pizzaModel.countDocuments;
+
+    return res.status(200).json(count)
+
+    } catch (error){
+
+        console.log("error"+ error)
+        return res.status(500).json({message: "internal Server Error"});
+    }
+
+};
+
+//buscar por nombre
+pizzaController.searchByName = async (req, res) => {
+    try {
+        //solicitar los datos
+        const {name} = req.body
+
+        const pizzas = await pizzaModel.find({
+            name: {$regex: name, $options: "i"}
+        })
+
+        if(!pizzas) {
+            return res.status(404).json({message: "Pizza no existe"})
+        }
+
+    } catch (error) {
+        console.log("error"+ error)
+        return res.status(500).json({message: "internal Server Error"});
+    }
+}
+
+
+
 export default pizzaController;
--- LittleCaesar20240528/backend/src/routes/pizza.js
+++ LittleCaesar20240528/backend/src/routes/pizza.js
@@ -8,8 +8,24 @@ router.route("/")
 .get(pizzaController.getPizzas)
 .post(pizzaController.insertPizza)
 
+router.route("/low-stock")
+.get(pizzaController.getLowStock)
+
+router.route("/price-range")
+.post(pizzaController.getPizzasByPriceRange)
+
+router.route("/cout")
+.get(pizzaController.countPizzas)
+
+router.route("/search-name")
+.post(pizzaController.searchByName)
+
 router.route("/:id")
 .put(pizzaController.updatePizza)
 .delete(pizzaController.deletePizza) 
+.get(pizzaController.getPizzasById)
+
+
+
 
 export default router
